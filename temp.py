1e1465dbdf2ccd84c7764f92c5dee4483b915428
Jonty <17radcj@olab.org.uk>
2023-04-25 22:36:49 +0100

    Working Bubble and Insertion Sort

--- temp.py
+++ temp.py
@@ -4,11 +4,13 @@ import time
 from random import randint
 
 array = []
+array2 = []
 lenarray = int(input("Enter length of array: "))
 for x in range(lenarray):
     array.append(randint(0,1000))
+    array2.append(randint(0,1000))
 
-array2 = array
+
 print("Bubble")
 bubTimeIn = time.time()
 bubble_sort(array)
@@ -21,3 +23,8 @@ insertion_sort(array2)
 inTimeFi = time.time()
 inTimeDiff = inTimeFi - inTimeIn
 print("Insertion Sort Time: ",inTimeDiff)
+
+
+#10000 = BS:4 IS:3
+#50000 = BS:107 IS:81
+#
