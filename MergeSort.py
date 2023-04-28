698035f80b9e64dc7b75c2e286c65f71afe6f30a
Jonty Radcliffe <17radcj@olab.org.uk>
2023-04-28 14:58:06 +0100

    MergeSort

---
+++ MergeSort.py
@@ -0,0 +1,41 @@
+
+def merge(A,B):
+    pA = 0
+    pB = 0
+    result = []
+    lenA = len(A)
+    lenB = len(B)
+    A.append(float("inf"))
+    B.append(float("inf"))
+    while pA < lenA or pB < lenB:
+        if A[pA] < B[pB]:
+            result.append(A[pA])
+            pA += 1
+        else:
+            result.append(B[pB])
+            pB += 1
+    return result
+
+def sort(A):
+    lenA = len(A)
+    if lenA == 1:
+        return A
+    left = sort(A[:lenA//2])
+    right = sort(A[lenA//2:])
+    return merge(left,right)
+
+arrayin = [1]
+if len(arrayin) == 0:
+    print("Error, Empty array entered.")
+    exit()
+
+array =[]
+for x in arrayin:
+    try:
+        array.append(int(x))
+    except:
+        print("Only integers are passed into array.")
+        pass
+
+print(sort(array))
+
--- temp.py
+++ temp.py
@@ -1,28 +1,39 @@
 from BubbleSortM import bubble_sort
 from InsertionSortM import insertion_sort
+from MergeSortM import merge_sort
 import time
 from random import randint
 
 array = []
 array2 = []
+array3 = []
 lenarray = int(input("Enter length of array: "))
 for x in range(lenarray):
     array.append(randint(0,1000))
     array2.append(randint(0,1000))
+    array3.append(randint(0,1000))
 
 
-print("Bubble")
+print("Bubble Sort:")
 bubTimeIn = time.time()
 bubble_sort(array)
 bubTimeFi = time.time()
 bubTimeDiff = bubTimeFi - bubTimeIn
 print("Bubble Sort Time: ",bubTimeDiff)
-print("Insertion")
+####
+print("Insertion Sort:")
 inTimeIn = time.time()
 insertion_sort(array2)
 inTimeFi = time.time()
 inTimeDiff = inTimeFi - inTimeIn
 print("Insertion Sort Time: ",inTimeDiff)
+####
+print("Merge Sort:")
+MeTimeIn = time.time()
+merge_sort(array3)
+MeTimeFi = time.time()
+MeTimeDiff = MeTimeFi - MeTimeIn
+print("Merge Sort Time: ",MeTimeDiff)
 
 
 #10000 = BS:4 IS:3
