a0992c677d7e7788acfc39922452c27ca2c89ba8
Jonty Radcliffe <17radcj@olab.org.uk>
2023-05-12 14:54:50 +0100

    started Dijkstras

--- DijkstrasAlgorithm.py
+++ DijkstrasAlgorithm.py
@@ -1,2 +1,33 @@
 from HeapPriorityQueue import queue
 from Graph import node
+
+
+graph = []
+inputting = True
+
+def ingraph(value):
+    count = 0
+    for x in graph:
+        if x == value:
+            return
+        else:
+            count += 1
+    return None
+
+while inputting:
+    Node = input("Enter Node: ")
+    neighbours = input("Enter neighbour/priority: ")
+    neighbours = neighbours.split(",")
+    for num in range(len(neighbours)):
+        neighbours[num] = neighbours[num].split("/")
+    
+    this_node = node(Node)
+    for neighbour in neighbours:
+        this_neighbour = node(neighbour[0])
+        if ingraph(this_neighbour):
+            
+
+
+
+Queue = queue(...)
+    
--- Graph.py
+++ Graph.py
@@ -4,11 +4,14 @@ class node():
     def __init__(self, selfvalue):
         self.value = selfvalue
         self.neighbours = []
+        self.weightedneighbours = []
         self.visited = False
         self.parent = None
         self.entry_time = None
         self.exit_time = None
         self.placeval = 0
+        
+
 
     def give_neighbour(self, neighbour):
         self.neighbours.append(neighbour)
