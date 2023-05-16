118172c0ef92b28ef250c76b44393b7d93b4f586
Jonty Radcliffe <17radcj@olab.org.uk>
2023-05-16 12:24:41 +0100

    Dijkstras not working

--- DijkstrasAlgorithm.py
+++ DijkstrasAlgorithm.py
@@ -60,24 +60,58 @@ Q.enqueue(graph[0], 0)
 beenDQ = []
 beenDQ_B = False
 
+runold = False
 
-print("------------")
-while Q.is_empty() == False:
+if runold == True:
+    print("------------")
+    while Q.is_empty() == False:
+        current = Q.dequeue()
+        for neighbour in current[0].neighbours:
+            if neighbour != None:
+                for x in beenDQ:
+                    if x == neighbour:
+                        beenDQ_B = True
+
+                if beenDQ_B == False:
+                    beenDQ.append(neighbour)
+                    num = current[0].getplaceofweight(neighbour)
+                    neighbour.placeval = current[0].placeval - int(current[0].weightofneighbours[num])
+                    neighbour.priority = neighbour.placeval
+                    Q.enqueue(neighbour, -neighbour.priority)
+                beenDQ.append(current[0])
+                beenDQ_B = False
+    
+        print(current[0].value,  current[0].placeval)
+
+###### new
+else:
+    print("------------")
+    while Q.is_empty() == False:
+        current = Q.dequeue()
+        for neighbour in current[0].neighbours:
+            if neighbour != None:
+                
+
+
+                for x in beenDQ:
+                    if x == neighbour:
+                        beenDQ_B = True
+
+                if beenDQ_B == False:
+                    beenDQ.append(neighbour)
+                    num = current[0].getplaceofweight(neighbour)
+                    neighbour.placeval = current[0].placeval - int(current[0].weightofneighbours[num])
+                    neighbour.priority = neighbour.placeval
+                    Q.enqueue(neighbour, -neighbour.priority)
+                beenDQ.append(current[0])
+                beenDQ_B = False
+    
+        print(current[0].value,  current[0].placeval)
+
+
+while Q.is_empty()==False:
     current = Q.dequeue()
     for neighbour in current[0].neighbours:
-        if neighbour != None:
-            for x in beenDQ:
-                if x == neighbour:
-                    beenDQ_B = True
-
-            if beenDQ_B == False:
-                beenDQ.append(neighbour)
-                num = current[0].getplaceofweight(neighbour)
-                neighbour.placeval = current[0].placeval - int(current[0].weightofneighbours[num])
-                neighbour.priority = neighbour.placeval
-                Q.enqueue(neighbour, -neighbour.priority)
-            beenDQ.append(current[0])
-            beenDQ_B = False
-  
-    print(current[0].value,  current[0].placeval)
-
+        neighbour.placeval
+        neighbour.priority = neighbour.placeval
+        Q.enqueue(neighbour, -neighbour.priority)
